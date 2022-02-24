4d57c822adf856720d746f8de37a3d1e4ab75527
Hur Abbas <100366779+Hur-Abbas@users.noreply.github.com>
2022-02-25 00:47:07 +0500

    Update JobListing.dart

--- lib/Screens/JobListing.dart
+++ lib/Screens/JobListing.dart
@@ -10,34 +10,37 @@ class JobListing extends StatelessWidget {
       resizeToAvoidBottomInset : false,
       backgroundColor: const Color(0xFF191720),
       body: Padding(
-        padding: const EdgeInsets.only(top: 110, left:27, right: 27,bottom: 59),
+        padding: const EdgeInsets.only(top: 79, left:27, right: 27),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Welcome\n", style:  TextStyle( color: Colors.white, fontSize: 18, fontWeight: FontWeight.w700,)),
-            const Text("Hur Abbas", style:  TextStyle( color: Colors.white, fontSize: 25, fontWeight: FontWeight.w300,)),
-            const SizedBox(height: 31),
-            const InputTextField(placeholder: "Enter position name"),
-            FractionallySizedBox(
-              alignment: Alignment.topCenter,
-            child: Container(
-              height: 260,
-              margin: const EdgeInsets.only(top:20),
-              padding: const EdgeInsets.only(left: 15),
-              decoration: BoxDecoration(
-                  color: const Color(0xFF1E1C24),
-                  border: Border.all(color:const Color(0xFF5D5D67)),
-                  borderRadius: BorderRadius.circular(15)
-              ),
-              child: const TextField(
-                style: TextStyle(color: Colors.white , fontSize: 15),
-                decoration: InputDecoration(hintText: "Describe Requirement...", hintStyle: TextStyle(color: Color(0xFF8F8F9E), fontSize: 15), border: InputBorder.none),
-              ),
+            const Text("Welcome", style:  TextStyle( color: Colors.white, fontSize: 18, fontWeight: FontWeight.w700,)),
+            Row(
+              children: const [
+                Text("Hur Abbas", style:  TextStyle( color: Colors.white, fontSize: 25, fontWeight: FontWeight.w300,)),
+                SizedBox(width: 162),
+                Icon(
+                  Icons.account_circle ,
+                  color: Colors.white,
+                  size: 26,
+                ),
+              ],
+            ),
+            const SizedBox(height: 19),
+          Container(
+            margin: const EdgeInsets.only(top:20),
+            padding: const EdgeInsets.only(top: 10, bottom: 10, left: 15),
+            decoration: BoxDecoration(
+                color: const Color(0xFF1E1C24),
+                border: Border.all(color:const Color(0xFF5D5D67)),
+                borderRadius: BorderRadius.circular(15)
+            ),
+            child: const TextField(
+              style: TextStyle(color: Colors.white, height: 1.5, fontSize: 15),
+              decoration: InputDecoration( prefixIcon: Icon(Icons.search, color: Color(0xFF5D5D67), size: 23), hintText: "Search keywords..", hintStyle: TextStyle(color: Color(0xFF8F8F9E), fontSize: 15), border: InputBorder.none),
             ),
           ),
-            const Spacer(),
-            const SizedBox(height: 11),
-            AppButton(label: "Submit Job", onPress: OnSubmitJobPress),
+
 
 
           ],
